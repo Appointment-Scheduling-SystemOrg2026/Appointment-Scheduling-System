abfeac1f367388e7e8870326472eb1199437267d
Tasneem kamal <s12113137@stu.najah.edu>
2026-04-01 23:58:04 +0300

    update

--- src/test/java/com/scheduling/observer/EmailSenderTest.java
+++ src/test/java/com/scheduling/observer/EmailSenderTest.java
@@ -2,10 +2,6 @@ package com.scheduling.observer;
 
 import org.junit.jupiter.api.Test;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Transport;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailSenderTest {
@@ -13,9 +9,17 @@ class EmailSenderTest {
     private final EmailSender sender =
             new EmailSender("test@gmail.com", "password");
 
-    
     @Test
-    void sendEmail_shouldReturnTrue_whenNoException() {
+    void constructor_shouldCreateInstance() {
+        EmailSender localSender =
+                new EmailSender("x@gmail.com", "123");
+
+        assertNotNull(localSender);
+    }
+
+    @Test
+    void sendEmail_shouldReturnBoolean_withoutCrashing() {
+        
         boolean result = sender.sendEmail(
                 "receiver@gmail.com",
                 "Test Subject",
@@ -23,40 +27,6 @@ class EmailSenderTest {
         );
 
         
-        assertNotNull(result);
-    }
-
-    
-    @Test
-    void sendEmail_shouldHandleInputCorrectly() {
-        boolean result = sender.sendEmail(
-                "a@b.com",
-                "Hello",
-                "Body"
-        );
-
         assertTrue(result == true || result == false);
-    }
-
-    
-    @Test
-    void sendEmail_shouldReturnBooleanValue() {
-        boolean result = sender.sendEmail(
-                "receiver@gmail.com",
-                "Test Subject",
-                "Test Body"
-        );
-
-       
-        assertTrue(result == true || result == false);
-    }
-
-    
-    @Test
-    void constructor_shouldSetFields() {
-        EmailSender localSender =
-                new EmailSender("x@gmail.com", "123");
-
-        assertNotNull(localSender);
     }
 }
